554901d169d1d67393e5d398e463a41c3cf1cff8
paymanzafar1 <91053236+paymanzafar1@users.noreply.github.com>
2026-05-25 11:18:18 +0330

    ADA 43th

--- client/src/app/menu/menu.component.ts
+++ client/src/app/menu/menu.component.ts
@@ -145,6 +145,22 @@ private buildIranScratchLinks (): MenuSection {
       })
     //}
 
+        base.links.push({
+        path: 'c/iranscratch4/video-playlists',
+        icon: 'subscriptions' as GlobalIconName,
+        label: $localize`:@@adaMessage9:Python programming`
+
+        base.links.push({
+        path: 'c/iranscratch5/video-playlists',
+        icon: 'subscriptions' as GlobalIconName,
+        label: $localize`:@@adaMessage10:GameMaker`
+
+        base.links.push({
+        path: 'c/iranscratch6/video-playlists',
+        icon: 'subscriptions' as GlobalIconName,
+        label: $localize`:@@adaMessage11:MIT App Inventor`
+
+
     return base
 }
 
